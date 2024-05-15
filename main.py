be176522d62fbbba921ea7b1a9e29afb2f7af760
Rafael Sonderegger <rafaelso@stanford.edu>
2024-05-15 17:19:36 +0200

    fully tested asynch with fast sampling and error handling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 import json
+import time
 from dataclasses import dataclass
 
 import numpy as np
@@ -12,46 +13,91 @@ MAX_CONSECUTIVE_FAILURES = 3
 NB_MEASUREMENTS_PER_SAMPLE = 3
 
 
-async def sample_sensor(sensor):
-    try:
-        sensor._connect()
-        measurements = []
-        for i in range(NB_MEASUREMENTS_PER_SAMPLE):
-            try:
-                new_measurement = sensor._sample()  # single sample from currently connected sensor
-                measurements.append(new_measurement.value)
-                await asyncio.sleep(1 / sensor.config.frequency)
-            except Exception as e:
-                print(f"Error sampling from sensor {sensor.sensor_id}: {e}")
-                sensor._disconnect()
-                sensor._connect()
-        sensor._disconnect()  # Current measurement of single sensor has been finished
-        measurement = np.mean(np.array(measurements))  # Averaged measurement
-
-    except Exception as e:
-        print(f"Could not connect to sensor {sensor.sensor_id}: {e}")
-        measurement = np.NaN
-    return measurement
-
-
 @dataclass
 class LoggerConfig:
     sensors_sampling_time: float
+    sensors_csv_file_period: int
 
 
 class Logger:
     def __init__(self, config: LoggerConfig):
         self.sensors_sampling_time = config.sensors_sampling_time
+        self.sensors_csv_file_period = config.sensors_csv_file_period
         self.sensor_list = []
+        self.connected_sensors = []
         self.sensor_data = {}
-        current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.output_sensors_file = None
 
     def set_sensor_list(self):
         config_dict = get_config_dict()
         self.sensor_list = get_sensor_list(config_dict)
 
-    async def start_logging(self):
-        await self.log_sensors_data()
+    def establish_sensor_connection(self):
+        count = 0
+        idx = []
+        for sensor in self.sensor_list:
+            try:
+                sensor._connect()
+                idx.append(count)
+            except Exception as e:
+                print(f"Could not connect to sensor {sensor.sensor_id}: {e}")
+            count += 1
+        self.connected_sensors = [self.sensor_list[i] for i in idx]
+        print(len(self.connected_sensors), "/", len(self.sensor_list), " sensors successfully connected.")
+        if len(self.connected_sensors) < len(self.sensor_list):
+            print(" Not found sensors are going to return NaN from here on.")
+
+    def disconnect_sensor_connection(self):
+        for sensor in self.sensor_list:
+            try:
+                sensor._disconnect()
+            except:
+                continue
+        print("Sensors disconnected ")
+
+    def sample_sensors(self):
+        for sensor in self.connected_sensors:
+            try:
+                print(sensor._sample(), f" sampled from: {sensor.sensor_id}")
+            except Exception as e:
+                print(f"Error sampling from sensor {sensor.sensor_id}: {e}")
+
+    async def run_logger(self):
+        batch = asyncio.gather(self.new_sensors_data_file(), self.log_sensors_data())
+        result_file, result_log_sensors = await batch
+
+    async def sample_sensor(self, sensor):
+        if sensor not in self.connected_sensors:
+            return np.NaN
+        try:
+            sample = sensor._sample()  # single sample from currently connected sensor
+            await asyncio.sleep(1 / sensor.config.frequency)
+        except Exception as e:
+            print(f"Error sampling from sensor {sensor.sensor_id}: {e}")
+            print("Trying to reconnect from sensor")
+            start_time = time.time()
+            try:
+                sensor._connect()
+            except Exception as e:
+                print(f"Could not connect to sensor {sensor.sensor_id}: {e}")
+            end_time = time.time()
+            diff_time = end_time - start_time
+            print(f"Reconnection attempt took {diff_time}")
+            sample = np.NaN
+        return sample
+
+    async def new_sensors_data_file(self):
+        while True:
+            if self.output_sensors_file is not None:
+                print("Searching cvs file to write to")
+            try:
+                print("Uploading to Drive PLACEHOLDER")
+            except:
+                print("Could not upload files to Google Drive, no Internet Connection.")
+            current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            self.output_sensors_file = f"./sensor_data_{current_datetime}.csv"
+            print(f" Creating new CSV Placeholder  at {current_datetime}")
+            await asyncio.sleep(self.sensors_csv_file_period)  # 3600 for one hour
 
     async def log_sensors_data(self):
         self.latest_sensor_data = {sensor: float for sensor in self.sensor_list}
@@ -59,9 +105,11 @@ class Logger:
         self.already_tried_reconnect = {sensor: 0 for sensor in self.sensor_list}
 
         while True:
-            tasks = [sample_sensor(sensor) for sensor in self.sensor_list]
+            # Wait for sampling of all sensors
+            tasks = [self.sample_sensor(sensor) for sensor in self.sensor_list]
             results = await asyncio.gather(*tasks)
 
+            # Gather all results in a single dict {sensor: val}
             for sensor, measurement in zip(self.sensor_list, results):
                 self.latest_sensor_data[sensor] = measurement
 
@@ -105,9 +153,6 @@ def get_config_dict():
     with open("./tmp1000_config.json") as config_file:
         config_dict["tmp1000"] = json.load(config_file)
 
-    with open("./lux1000.json") as config_file:
-        config_dict["lux1000"] = json.load(config_file)
-
     with open("./pre1000_config.json") as config_file:
         config_dict["pre1000"] = json.load(config_file)
 
@@ -120,7 +165,9 @@ def get_logger_config(config_file_path):
     with open(config_file_path) as config_file:
         config = json.load(config_file)
     print("Logger config loaded successfully.")
-    logconfig = LoggerConfig(sensors_sampling_time=config.get("sensors_sampling_time", 0.0))
+    logconfig = LoggerConfig(
+        sensors_sampling_time=config["sensors_sampling_time"], sensors_csv_file_period=config["sensors_csv_file_period"]
+    )
     return logconfig
 
 
@@ -132,4 +179,12 @@ if __name__ == "__main__":
     Log = Logger(config)
     print("Setting sensor list...")
     Log.set_sensor_list()
-    asyncio.run(Log.start_logging())
+
+    print("Establish sensor connections...")
+    Log.establish_sensor_connection()
+
+    print(" Single test measurement from each connected sensor: ")
+    Log.sample_sensors()
+
+    print("---------- Start Logging ----------")
+    asyncio.run(Log.run_logger())
